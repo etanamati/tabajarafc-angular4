b92ecab3e89d0513550f626c09509649eeb22dfa
Elton Tanamati <elton.tanamati@elotech.localnet>
2017-03-29 13:02:54 -0300

    Criação teste Clube - Create Component

--- src/app/modules/clube/clube.component.spec.ts
+++ src/app/modules/clube/clube.component.spec.ts
@@ -1,22 +1,29 @@
+import { AppRoutingModule } from './../../app.routing';
+import { HttpModule } from '@angular/http';
+import { ClubeModule } from './clube.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClubeComponent } from './clube.component'
-describe('TimeComponent', () => {
+import { APP_BASE_HREF } from '@angular/common';
+
+fdescribe('TimeComponent', () => {
   let component: ClubeComponent;
   let fixture: ComponentFixture<ClubeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ClubeComponent ]
+      imports: [ClubeModule, HttpModule, AppRoutingModule],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    });
+    
+    TestBed.compileComponents().then(()=> {
+      fixture = TestBed.createComponent(ClubeComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     })
-    .compileComponents();
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ClubeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
